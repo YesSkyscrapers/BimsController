39d3a6871703b2b79c3b6bad7bb48968b7cf2ada
YesSkyscrapers <yes.skyscrapers@gmail.com>
2019-07-11 03:30:36 +0300

    Revert "Online checker logis creation started"
    This reverts commit 46818d1786e78662b50ab3170f229f3058e366d9.

--- BimsController/Logics/Bot/Bot.cs
+++ BimsController/Logics/Bot/Bot.cs
@@ -1,22 +1,16 @@
 ﻿using BimsController.Defines;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MessageBox = System.Windows.MessageBox;
 
 namespace BimsController.Logics.Bot
 {
     public class Bot
     {
-        private string AUTORECONNECT_AUTH_URL = "https://cp.uwow.biz/auth/login";
-
         public bool isRunning = false;
-        public SessionInfo[] Infos = new SessionInfo[] { new SessionInfo(), new SessionInfo(), new SessionInfo() };
+        public ProcessState[] States = new ProcessState[] { ProcessStates.Stopped, ProcessStates.Stopped, ProcessStates.Stopped };
 
         public void SwitchBot()
         {
@@ -34,30 +28,12 @@ namespace BimsController.Logics.Bot
                     logic.settings.appSettings.CloseAppSettingsWindow();
                 logic.settings.windowSettings.isLockSettingsWindow = true;
 
-                int profilesCountWithEnabledAutoreconnectOption = logic.settings.appSettings.profilesSettings.Select(profile => profile.autoReconnect).Where(enabled => enabled).Count();
-                if (profilesCountWithEnabledAutoreconnectOption > 0)
-                    MessageBox.Show("Please, fill captcha field in " + profilesCountWithEnabledAutoreconnectOption + " chrome windows");
-
-
-                //parallel run
-                await Task.WhenAll((new List<int>() { 0, 1, 2 }).Where(i => (id == i || id == -1)).Select(i =>
-                    StartSession(i)).ToArray());
-
-                //posled run
-                /*
                 for (int i = 0; i < 3; i++)
                 {
                     if (logic.settings.appSettings.profilesSettings[i].enabled && (id == i || id == -1))
                         StartSession(i);
                 }
-                */
             });
-        }
-
-        private async Task SetStateWithDelay(int sessionId, ProcessState state)
-        {
-            Logic.Execute(_ => SetState(sessionId, state));
-            await Task.Delay(100);
         }
 
         private async Task StartSession(int sessionId)
@@ -65,106 +41,40 @@ namespace BimsController.Logics.Bot
             SetState(sessionId, ProcessStates.WaitingRunning);
             await Logic.ExecuteAsync(async (logic) =>
             {
-                if (logic.settings.appSettings.profilesSettings[sessionId].autoReconnect)
-                {
-                    await SetStateWithDelay(sessionId, ProcessStates.OpeningOnlineChecker);
-
-                    Infos[sessionId].AutoreconnectLoopingClosed = false;
-
-                    ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-                    service.HideCommandPromptWindow = true;
-
-                    Infos[sessionId].WebDriver = new ChromeDriver(service);
-                    Infos[sessionId].WebDriver.Navigate().GoToUrl(AUTORECONNECT_AUTH_URL);
-
-                    await SetStateWithDelay(sessionId, ProcessStates.EnteringCredentialsToOnlineChecker);
-
-                    Infos[sessionId].WebDriver.FindElement(By.Name("username")).SendKeys(logic.settings.appSettings.profilesSettings[sessionId].login);
-                    Infos[sessionId].WebDriver.FindElement(By.Name("password")).SendKeys(logic.settings.appSettings.profilesSettings[sessionId].password);
-                    Infos[sessionId].WebDriver.FindElement(By.ClassName("lbl")).Click();
-                    Infos[sessionId].WebDriver.FindElement(By.Name("captcha")).Click();
-
-                    await SetStateWithDelay(sessionId, ProcessStates.WaitingCaptchaFilling);
-
-                    while (Infos[sessionId].WebDriver.Url == AUTORECONNECT_AUTH_URL)
-                    {
-                        await Task.Delay(1000);
-                        if (Infos[sessionId].State.Equals(ProcessStates.Canceled))
-                        {
-
-                            Infos[sessionId].AutoreconnectLoopingClosed = true;
-                            return;
-                        }
-                    }
-
-                    Infos[sessionId].WebDriver.Manage().Window.Position = new System.Drawing.Point(-32000, -32000);
-                }
+                await Task.Delay(1000 * sessionId);
+                if (States[sessionId].Equals(ProcessStates.Canceled))
+                    return;
+                SetState(sessionId, ProcessStates.Running);
             });
-
-            if (Infos[sessionId].State.Equals(ProcessStates.Canceled))
-            {
-
-                Infos[sessionId].AutoreconnectLoopingClosed = true;
-                return;
-            }
-
-            Logic.Execute(logic => SetState(sessionId, ProcessStates.Running));
-
-            if (Infos[sessionId].WebDriver != null)
-            {
-                _ = Logic.ExecuteAsync(async logic =>
-                {
-                    Infos[sessionId].AutoreconnectCheckingEnabled = true;
-
-                    while (Infos[sessionId].AutoreconnectCheckingEnabled)
-                    {
-                        //actions
-                        await Task.Delay(100);
-                    }
-
-                    Infos[sessionId].CloseWebDriver();
-                    Infos[sessionId].AutoreconnectLoopingClosed = true;
-                });
-            }
         }
-
-        
 
         private async Task StopSession(int sessionId)
         {
             SetState(sessionId, ProcessStates.Canceled);
-            
-            if (Infos[sessionId].WebDriver != null)
-            {
-                Infos[sessionId].CloseWebDriver();
-            }
-
             await Logic.ExecuteAsync(async (logic) =>
             {
-                if (logic.settings.appSettings.profilesSettings[sessionId].autoReconnect)
-                    while (!Infos[sessionId].AutoreconnectLoopingClosed)
-                        await Task.Delay(200);
+                await Task.Delay(1000 * sessionId);
                 SetState(sessionId, ProcessStates.Stopped);
             });
         }
 
         public void SetState(int sessionId, ProcessState state)
         {
-            Infos[sessionId].State = state;
+            States[sessionId] = state;
         }
 
         public async void Stop(int id = -1)
         {
             Logic.Execute(logic => logic.settings.windowSettings.isStartButtonLocked = true);
-            _ = Logic.ExecuteAsync(async logic =>
-              {
+            Logic.ExecuteAsync(async logic =>
+            {
                 //antiplaying delay - idk why...just safety
                 await Task.Delay(3000);
-                  logic.settings.windowSettings.isStartButtonLocked = false;
-              });
+                logic.settings.windowSettings.isStartButtonLocked = false;
+            });
             await Logic.ExecuteAsync(async logic => {
                 
-                await Task.WhenAll((new List<int>() { 0, 1, 2 }).Where(i => !Infos[i].State.Equals(ProcessStates.Stopped) && (id == i || id == -1)).Select(i => 
+                await Task.WhenAll((new List<int>() { 0, 1, 2 }).Where(i => !States[i].Equals(ProcessStates.Stopped) && (id == i || id == -1)).Select(i => 
                 StopSession(i)).ToArray());
 
                 //Check for locking or unlocking settings window
@@ -174,7 +84,7 @@ namespace BimsController.Logics.Bot
                 {
                     if (i == id)
                         continue;
-                    if (!Infos[i].State.Equals(ProcessStates.Stopped))
+                    if (!States[i].Equals(ProcessStates.Stopped))
                         isAllProcessesStopped = false;
                 }
 
